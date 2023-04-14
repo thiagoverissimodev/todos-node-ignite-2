09b5b88b6a3eb584d0896a664b7047b95bf3f224
Thiago Verissimo <thiagovsc96@gmail.com>
2023-04-14 18:14:39 -0400

    second-version-todos

--- src/index.js
+++ src/index.js
@@ -27,19 +27,50 @@ function checksCreateTodosUserAvailability(request, response, next) {
   const { user } = request
 
   if(user.pro === false && user.todos.length >= 10){
-    return response.status(400).json({ error: 'Task limit reaching. Change your plan to Pro!' });
+    return response.status(403).json({ error: 'Task limit reaching. Change your plan to Pro!' });
   }
 
   return next()
-
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  const {id} = request.params
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  if(!validate(id)){
+    return response.status(400).json({error:"ID invalid"})
+  }
+
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error:"Todo not found"})
+  }
+
+  request.user = user
+  request.todo = todo
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  const user = users.find((user) => user.id === id)
+
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
